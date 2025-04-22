61824e28dc80045e8d8ca999091a7198f6669c6a
Brahim Ben Younes <37020430+BrahimBenyounes@users.noreply.github.com>
2025-04-22 22:34:15 +0100

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@
 //ng serve --configuration production
 export const environment = {
   production: true,
-  baseUrl: 'http://production:8080',
+  //baseUrl: 'http://production:8080',
+  baseUrl: 'http://192.168.186.128:8080',
 };
